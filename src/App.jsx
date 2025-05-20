8cd80002217c36d90279bec6bb5385e20dbb400e
yesicablum <yesica.blum@uniminuto.edu.co>
2025-05-20 14:12:53 -0500

    arreglo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Register from './pages/Register';
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import "./App.css";
+import Login from "./pages/Login";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Bookings from "./pages/Bookings";
+import Services from "./pages/Services";
+import Clients from "./pages/Clients";
+import AdminServices from "./pages/AdminServices";
 
 function App() {
-
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="register/" element={<Register />} />
-      </Routes>
+      <div className="container">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/bookings" element={<Bookings />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/clientes" element={<Clients />} />
+          <Route path="/adminservicios" element={<AdminServices />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
-
-  )
+  );
 }
 
-export default App
+export default App;
